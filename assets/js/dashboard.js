93daaa65a87b6142bc0de5d6bff0f2d5146fe38b
romeoscript <romeobourne211@gmail.com>
2022-07-25 18:52:20 +0100

    dashboard

---
+++ assets/js/dashboard.js
@@ -0,0 +1,53 @@
+const showContent = document.querySelector('.btn-kid')
+const showContent2 = document.querySelector('.btn-kid2')
+const showContent3 = document.querySelector('.btn-kid3')
+const displayItem = document.querySelector('.kid-first')
+const displayItem2 = document.querySelector('.kid-sec')
+const displayItem3 = document.querySelector('.kid-third')
+
+showContent.addEventListener('click',()=>{
+    displayItem.style.display = 'block'
+    displayItem2.style.display = 'none'
+    displayItem3.style.display = 'none'
+})
+showContent2.addEventListener('click',()=>{
+    displayItem.style.display = 'none'
+    displayItem2.style.display = 'block'
+    displayItem3.style.display = 'none'
+})
+showContent3.addEventListener('click',()=>{
+    displayItem.style.display = 'none'
+    displayItem2.style.display = 'none'
+    displayItem3.style.display = 'block'
+})
+
+// preloader
+const loader = document.getElementById("preloader");
+window.addEventListener("load", () => {
+  loader.style.display = "none";
+});
+
+
+
+//   betslipsection
+let betClick = document.getElementById("bet");
+let betSlipt = document.querySelector(".betslip");
+let svgClick = document.querySelector(".fill");
+let backDrop = document.querySelector('.infodrop')
+
+betClick.addEventListener("click", clicked);
+function clicked() {
+  betSlipt.style.display = "block";
+  betClick.style.display = "none";
+  backDrop.style.display = 'block'
+}
+backDrop.addEventListener('click',click)
+svgClick.addEventListener("click", click);
+function click() {
+  betSlipt.style.display = "none";
+  betClick.style.display = "block";
+  backDrop.style.display= 'none'
+}
+
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,41 +4,7 @@ window.addEventListener("load", () => {
   loader.style.display = "none";
 });
 
-// swiper
 
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
-    520: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
-});
 
 //   betslipsection
 let betClick = document.getElementById("bet");
@@ -59,3 +25,5 @@ function click() {
   backDrop.style.display= 'none'
 }
 backDrop.addEventListener('click',click)
+
+
